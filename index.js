425e253a07c4616fa59fea5f64dfa2b0789f3456
Code-andCreate <createandcode0eesa@gmail.com>
2024-11-24 15:16:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const inputField = document.querySelector('.no');
+const buttons = document.querySelectorAll('button');
+//expression
+let expression = '';
 
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.innerText;
+//conditions for buttons
+        if (value === 'C') {
+            expression = '';
+        } else if (value === '=') {
+            try {
+                expression = eval(expression).toString();
+            } catch {
+                expression = 'Error';
+            }
+        } else {
+            expression += value;
+        }
+
+        inputField.value = expression;
+    });
+});
